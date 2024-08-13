cf2b4f7eee4ae2c248553344dd075b84b1b2db64
marufBS <0909maruf@gmail.com>
2024-08-14 00:30:20 +0600

    card added

--- src/client/pages/products/products.jsx
+++ src/client/pages/products/products.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 
-const Products = () => {
+export default function Products() {
+  const [products, setProducts] = useState([])
+  const [imageData, setImageData] = useState([])
+  const list = [
+    {
+      title: "Orange",
+      img: "/images/fruit-1.jpeg",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "/images/fruit-2.jpeg",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "/images/fruit-3.jpeg",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "/images/fruit-4.jpeg",
+      price: "$5.30",
+    },
+    {
+      title: "Avocado",
+      img: "/images/fruit-5.jpeg",
+      price: "$15.70",
+    },
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      price: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+  ];
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then((res) => {
+        setImageData([...res.data])
+      })
+      .then(() => {
+        list.map((obj, i) => obj.img = imageData[i]?.url)
+        setProducts(list)
+        console.log(list)
+      })
+
+  }, [])
+  // list.map((obj, i) => obj.img = imageData[i]?.url)
+  // setProducts(list)
+  // for (let index = 0; index < 10; index++) {
+  // }
+  // console.log(products)
+
+
+
+
   return (
-    <div>Productsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoductsoducts</div>
-  )
+    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      {products.map((item, index) => (
+        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="lg"
+              width="100%"
+              alt={item.title}
+              className="w-full object-cover h-[140px]"
+              src={item.img}
+            />
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>{item.title}</b>
+            <p className="text-default-500">{item.price}</p>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
-
-export default Products
