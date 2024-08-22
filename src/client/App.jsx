fd40bad21b7a0a77afe2b9ca6e6806d8efd31ae8
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-08-22 21:00:17 +0600

    Update App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -14,38 +14,38 @@ import ViewOrder from "./pages/inventory/viewOrder";
 import { Accordion, AccordionItem, Divider, Listbox, ListboxItem } from "@nextui-org/react";
 import SidebarExpand from "./components/sidebars/sidebarExpand";
 function App() {
-  const mainRef = useRef(null)
-  const dispatch = useDispatch()
-  const mainHeight = useSelector((state) => state.app.mainHeight)
+  // const mainRef = useRef(null)
+  // const dispatch = useDispatch()
+  // const mainHeight = useSelector((state) => state.app.mainHeight)
   const darkTheme = useSelector((state) => state.app.darkTheme)
-  useEffect(() => {
-    dispatch(setMainHeight(mainRef.current.offsetHeight))
-  }, [])
-  console.log(mainHeight)
+  // useEffect(() => {
+  //   dispatch(setMainHeight(mainRef.current.offsetHeight))
+  // }, [])
+  // console.log(mainHeight)
 
   return (
-    <main ref={mainRef} className={`flex ${darkTheme?'dark':'light'} text-foreground bg-background h-dvh`}>
-      <div className="flex-1 flex flex-row">
-
+  <>
+    {/* <main ref={mainRef} className={`flex ${darkTheme ? 'dark' : 'light'} text-foreground bg-background mrf h-screen overflow-hidden`}>
+    </main> */}
+    <main className={`${darkTheme ? 'dark' : 'light'} bg-background text-foreground flex flex-col h-screen overflow-hidden`}>
+      <Appbar />
+      <div className="flex flex-row flex-1 h-0">
         <SidebarExpand />
-        <div className="flex flex-col w-full">
-          <Appbar />
-          <div style={{ height: mainHeight - 65 }} className="overflow-auto">
-            <Routes>
-              <Route path="/" element={<Products />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/customers" element={<Customer />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/inventory/viewOrder/:id" element={<ViewOrder />} />
-              <Route path="/inventory/createOrder" element={<CreateOrder />} />
-              {/* <Route path="/signup" element={<SignUp />} />
+        <div className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/customers" element={<Customer />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/inventory/viewOrder/:id" element={<ViewOrder />} />
+            <Route path="/inventory/createOrder" element={<CreateOrder />} />
+            {/* <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} /> */}
-            </Routes>
-          </div>
+          </Routes>
         </div>
       </div>
-
     </main>
+  </>
   );
 }
 
